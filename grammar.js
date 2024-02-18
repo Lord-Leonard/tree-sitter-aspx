09b82464c701358dc9f9a21442f19f528947a0d7
Leonard Georg <l.georg@weisenburger.de>
2024-02-18 16:08:12 +0100

    feat(parser): add version 1

--- grammar.js
+++ grammar.js
@@ -4,10 +4,58 @@
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
 
-export default grammar({
+module.exports =  grammar({
   name: 'aspx',
 
   rules: {
-    
+    template: $ => repeat(choice(
+      $.embedded_code_block,
+      $.display_expression,
+      $.directive,
+      $.data_binding,
+      $.data_binding,
+      $.comment_expression,
+      $.content
+    )),
+
+    code: $ => repeat1(choice(/[^%=_-]+|[%=_-]/, '%%>')),
+
+    content: $ => prec.right(repeat1(choice(/[^<]+|</, '<%%'))),
+
+    embedded_code_block: $ => seq(
+      choice('<%'),
+      optional($.code),
+      choice('%>')
+    ),
+
+    display_expression: $ => seq(
+      choice('<%='),
+      optional($.code),
+      choice('%>')
+    ),
+
+    directive: $ => seq(
+      choice('<%@'),
+      optional($.code),
+      choice('%>')
+    ),
+
+    data_binding: $ => seq(
+      choice('<%#'),
+      optional($.code),
+      choice('%>')
+    ),
+
+    expression_builder: $ => seq(
+      choice('<%$'),
+      optional($.code),
+      choice('%>')
+    ),
+
+    comment_expression: $ => seq(
+      choice('<%--'),
+      optional(alias($.code, $.comment)),
+      choice('--%>')
+    ),
   }
 });
