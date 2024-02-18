b09788f48eb286102c852fa8e77127370ea58324
Leonard Georg <l.georg@weisenburger.de>
2024-02-18 12:33:14 +0100

    initial commit

---
+++ grammar.js
@@ -0,0 +1,13 @@
+/* eslint-disable arrow-parens */
+/* eslint-disable camelcase */
+/* eslint-disable-next-line spaced-comment */
+/// <reference types="tree-sitter-cli/dsl" />
+// @ts-check
+
+export default grammar({
+  name: 'aspx',
+
+  rules: {
+    
+  }
+});
